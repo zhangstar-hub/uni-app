c2437daeb4bcc3bcb61b73f6b414d5ad89e25a8e
zxx <1716011262@qq.com>
2023-03-09 16:54:40 +0800

    完成GoodsDetail

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,35 @@
 <template>
-	<view>
-		
+	<view v-if="goods_info.goods_name" class="goods-detail-container">
+		<!-- 轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="(item, index) in goods_info.pics" :key="index">
+				<image :src="item.pics_big" mode="" @click="perview(index)"></image>
+			</swiper-item>
+		</swiper>
+		<view class="goods-info-box">
+			<!-- 商品价格 -->
+			<view class="price">
+				¥{{goods_info.goods_price}}
+			</view>
+			<!-- 信息主体区域 -->
+			<view class="goods-info-body">
+				<view class="goods-name">
+					{{goods_info.goods_name}}
+				</view>
+				<view class="favi">
+					<uni-icons type="star" size="18" color="gray"></uni-icons>
+					<text>收藏</text>
+				</view>
+			</view>
+			<!-- 运费 -->
+			<view class="yf">
+				快递：免运费
+			</view>
+		</view>
+		<rich-text :nodes="goods_info.goods_introduce"></rich-text>
+		<view class="goods-nav">
+			<uni-goods-nav :fill="true"  :options="options" :buttonGroup="buttonGroup"  @click="onClick" @buttonClick="buttonClick" />
+		</view>
 	</view>
 </template>
 
@@ -8,12 +37,108 @@
 	export default {
 		data() {
 			return {
-				
+				goods_info: {},
+				options: [{
+					icon: 'shop',
+					text: '店铺',
+					infoBackgroundColor:'#007aff',
+					infoColor:"red"
+				}, {
+					icon: 'cart',
+					text: '购物车',
+					info: 2
+				}],
+				buttonGroup: [{
+				  text: '加入购物车',
+				  backgroundColor: '#ff0000',
+				  color: '#fff'
+				},
+				{
+				  text: '立即购买',
+				  backgroundColor: '#ffa200',
+				  color: '#fff'
+				}
+				]
 			};
+		},
+		onLoad(options) {
+			console.log(options);
+			this.getGoodsDetail(options.goods_id);
+		},
+		methods: {
+			async getGoodsDetail(goods_id){
+				const {data:res} = await uni.$http.get("/api/public/v1/goods/detail", {goods_id})
+				if(res.meta.status !== 200) return uni.$showMsg()
+				res.message.goods_introduce = res.message.goods_introduce.replace(
+				/<img /g, '<img style="display: block"'
+				).replace(/webp/g, "jpg")
+				this.goods_info = res.message
+			},
+			perview(index){
+				uni.previewImage({
+					current: index,
+					urls: this.goods_info.pics.map(x => x.pics_big)
+					
+				})
+			},
+			onClick(e){
+				if(e.content.text === "购物车") {
+					uni.switchTab({
+						url: "/pages/cart/cart"
+					})
+				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	swiper {
+		height: 750rpx;
+	}
+	image{
+		width: 100%;
+		height: 100%;
+	}
+	.goods-info-box {
+		padding: 10px;
+		.price {
+			color: #C00000;
+			font-size: 18px;
+			margin: 10px 0;
+		}
+		.goods-info-body {
+			display: flex;
+			justify-content: space-between;
+			
+			.goods-name {
+				font-size: 13px;
+				margin-right: 10px;
+			}
+			.favi {
+				width: 120px;
+				font-size: 12px;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				border-left: 1px solid #efefef;
+				color: gray;
+			}
+		}
+		.yf {
+			font-size: 12px;
+			color: gray;
+			margin: 10px 0;
+		}
+	}
+	.goods-nav {
+		position: fixed;
+		bottom: 0px;
+		left: 0px;
+		width: 100%;
+	}
+	.goods-detail-container {
+		padding-bottom: 50px;
+	}
 </style>
